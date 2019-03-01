482e3ebc9eeeb12d237a80a5921b5de3975b571d
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2019-03-01 10:03:58 +0100

    Cleanup.
    The favicon partial is generated when building.

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ const staticDir = lazyValue(() => (hugoConfig['staticDir'] || 'static') + '/');
  * @returns The parsed config
  */
 function readConfig() {
-	return toml.parse(fs.readFileSync('config.toml', 'utf8'));
+    return toml.parse(fs.readFileSync('config.toml', 'utf8'));
 }
 
 /**
@@ -47,8 +47,7 @@ gulp.task('dev', done => execHugoTask(['server'], done));
 
 // Start the hugo build task
 gulp.task('build', done => {
-	//process.env.HUGO_ENV = "production";
-	execHugoTask([], done)
+    execHugoTask([], done)
 });
 
 // Print the hugo version
@@ -56,10 +55,10 @@ gulp.task('hugo:version', done => execHugoTask(['version'], done));
 
 // Generate github related configuration files.
 gulp.task('github-setup', function (done) {
-	const data = readConfig();
+    const data = readConfig();
     // Generate the CNAME file
     fs.writeFileSync(publishDir() + 'CNAME', data['baseURL'].replace(/https?:\/\//, ''));
-	// Generate the config file to define the 404 page file.
+    // Generate the config file to define the 404 page file.
     fs.writeFileSync(publishDir() + '_config.yaml', 'permalink: /404.html\n');
     done();
 });
@@ -68,40 +67,40 @@ const faviconDataFile = 'faviconData.json';
 
 // Generate/update the favicon header info partial.
 gulp.task('favicon:generate-header-info', function() {
-	require('./favicon_config');
-	const faviconInfoDir = 'layouts/partials/';
-	if (!fs.existsSync(faviconInfoDir)) mkdirp.sync(faviconInfoDir);
+    require('./favicon_config');
+    const faviconInfoDir = 'layouts/partials/';
+    if (!fs.existsSync(faviconInfoDir)) mkdirp.sync(faviconInfoDir);
     const faviconInfo = faviconInfoDir + config['partial'];
     // Regenerate the file with a informational header.
     fs.writeFileSync(faviconInfo, '<!-- Do not modify the favicon information manually. -->\n');
     // Inject favicon data into the generated file.
     return gulp.src(faviconInfo)
-		.pipe(favicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(faviconDataFile)).favicon.html_code))
-		.pipe(gulp.dest(faviconInfoDir))
+        .pipe(favicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(faviconDataFile)).favicon.html_code))
+        .pipe(gulp.dest(faviconInfoDir))
 });
 
 // Generate the icons. This task takes a few seconds to complete.
 gulp.task('favicon:generate', function(done) {
-	require('./favicon_config');
-	const logo = config['logo'];
+    require('./favicon_config');
+    const logo = config['logo'];
 
-	config['masterPicture'] = config['masterPicture'] || staticDir() + logo;
-	if (!config['dest']) {
-		const index = logo.lastIndexOf('/');
-		const dir = logo.substring(0, index) + '/favicon';
-		config['dest'] = index === -1 ? '' : staticDir() + dir;
-		config['iconsPath'] = config['iconsPath'] || '/' + dir;
-	} else {
-		config['iconsPath'] = config['iconsPath'] || '/';
-	}
-	config['settings'] = config['settings'] || {
-		scalingAlgorithm: 'Mitchell',
-		errorOnImageTooSmall: false,
-		readmeFile: false,
-		htmlCodeFile: false,
-		usePathAsIs: false
-	};
-	config['markupFile'] = faviconDataFile;
+    config['masterPicture'] = config['masterPicture'] || staticDir() + logo;
+    if (!config['dest']) {
+        const index = logo.lastIndexOf('/');
+        const dir = logo.substring(0, index) + '/favicon';
+        config['dest'] = index === -1 ? '' : staticDir() + dir;
+        config['iconsPath'] = config['iconsPath'] || '/' + dir;
+    } else {
+        config['iconsPath'] = config['iconsPath'] || '/';
+    }
+    config['settings'] = config['settings'] || {
+        scalingAlgorithm: 'Mitchell',
+        errorOnImageTooSmall: false,
+        readmeFile: false,
+        htmlCodeFile: false,
+        usePathAsIs: false
+    };
+    config['markupFile'] = faviconDataFile;
 
     favicon.generateFavicon(config, done);
 });
@@ -109,11 +108,11 @@ gulp.task('favicon:generate', function(done) {
 // Check for updates on RealFaviconGenerator (think: Apple has just
 // released a new Touch icon along with the latest version of iOS).
 gulp.task('favicon:check-for-update', function(done) {
-	const currentVersion = JSON.parse(fs.readFileSync(faviconDataFile)).version;
+    const currentVersion = JSON.parse(fs.readFileSync(faviconDataFile)).version;
     favicon.checkForUpdates(currentVersion, function(err) {
-		if (err) throw err;
-		done()
-	});
+        if (err) throw err;
+        done()
+    });
 });
 
 // Run all favicon tasks
